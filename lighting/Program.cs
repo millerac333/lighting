63d49aca220713b861ddb93b872afae5c038a8f9
Aaron Miller <millerac333@gmail.com>
2018-08-27 14:30:10 -0500

    added to student list and instructor list

--- lighting/Program.cs
+++ lighting/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Create some cohorts, students, and instructors in your `Program.cs` and assign the students and instructors to the cohort
+
+using System;
 using System.Collections.Generic;
 
 namespace Lighting
@@ -7,7 +9,26 @@ namespace Lighting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cohort twentySix = new Cohort()
+            { CohortNumber = 26 };
+
+            Instructor steve = new Instructor("Steve", "Brownlee");
+            Instructor jisie = new Instructor("Jisie", "David");
+            Instructor emily = new Instructor("Emily", "Lemmon");
+            Instructor jordan = new Instructor("Jordan", "Castelloe");
+
+            Student aaron = new Student("Aaron", "Miller");
+            Student shu = new Student("Shu", "Sajid");
+            Student jewel = new Student("Jewel", "Ramierez");
+
+            twentySix.InstructorList.Add(steve);
+            twentySix.InstructorList.Add(jisie);
+            twentySix.InstructorList.Add(emily);
+            twentySix.InstructorList.Add(jordan);
+
+            twentySix.StudentList.Add(aaron);
+            twentySix.StudentList.Add(shu);
+            twentySix.StudentList.Add(jewel);
         }
     }
 }
