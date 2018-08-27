939275532affb8cd2fdc6a5d58d2009ede3f4f61
Aaron Miller <millerac333@gmail.com>
2018-08-27 14:05:23 -0500

    created instructor class

--- lighting/Instructor.cs
+++ lighting/Instructor.cs
@@ -1,27 +1,24 @@
-// Create a `Student` class with the following properties and fields
+// Create an `Instructor` class with the following properties and fields
 using System;
 using System.Collections.Generic;
 
 namespace lighting
 {
-    public class Student
+    public class Instructor
     {
         // 1. private fields for first name and last name
         private string _firstName;
         private string _lastName;
 
-        // 2. Public calculated property that returns "Firstname Lastname" of student
+        // 2. Public calculated property that returns "Firstname Lastname" of instructor
         public string Name { get => $"{_firstName} {_lastName}"; }
 
         // 3. Constructor that accepts first name and last name
-        public Student(string firstName, string lastName)
+        public Instructor(string firstName, string lastName)
         {
             _firstName = firstName;
             _lastName = lastName;
         }
-
-        // 4. A public list of exercises
-        public List<Excercise> AssignedExcercises { get; } = new List<Excercise>();
 
     }
 }
