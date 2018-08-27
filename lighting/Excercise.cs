82cae0e96f271ba495ff5763d1035c48bca69fef
Aaron Miller <millerac333@gmail.com>
2018-08-27 13:34:12 -0500

    first commit

---
+++ lighting/Excercise.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace lighting
+{
+    public class Excercise
+    {
+        public string name { get; set; }
+        public string gitHubURL { get; set; }
+        public string language { get; set; }
+
+    }
+}
---
+++ lighting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lighting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
